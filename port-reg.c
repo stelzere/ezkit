744a5a4915c9f0240be383758fa19091ead11c2a
Elwin Stelzer Eliazer <stelzere@gmail.com>
2015-03-26 23:57:16 -0700

    filled in the functions

--- port-reg.c
+++ port-reg.c
@@ -1,14 +1,58 @@
-#define PORT_ENABLE 0x1000
+#define PORT_ENABLE_REG_ADDR (0x1000)
 
+#define MAX_PORT_NUM (31)
+
+#define ERROR (-1)
+#define SUCCESS (0)
+
+typedef unsigned long uint32;
+
+
+
+/** @brief Sets the port_enable_reg for a given port_num
+ *
+ *  @param port_num the switch port number (0 <= port_num <= 31)
+ *  @return SUCCESS or ERROR
+ */
 int
-set_port_reg(int port_num, int val)
+set_port_enable_reg(int port_num)
 {
+
+  uint32 volatile * const port_enable_reg = (uint32 *) PORT_ENABLE_REG_ADDR;
+
   if ((port_num < 0) || 
-      (port_num > 31) ||
-      (val < 0) ||
-      (val > 1)) {
-    return -1; // or assert
+      (port_num > MAX_PORT_NUM)) {
+    return ERROR;
   }
 
+  *port_enable_reg |= (1u << port_num); // need to add lock/unlock if RMW not atomically done
+
+  return SUCCESS;
 		       
 }
+
+
+
+/** @brief Clears the port_enable_reg for a given port_num
+ *
+ *  @param port_num the switch port number (0 <= port_num <= 31)
+ *  @return SUCCESS or ERROR
+ */
+int
+clear_port_enable_reg(int port_num)
+{
+
+  uint32 volatile * const port_enable_reg = (uint32 *) PORT_ENABLE_REG_ADDR;
+
+  if ((port_num < 0) || 
+      (port_num > MAX_PORT_NUM)) {
+    return ERROR;
+  }
+
+  *port_enable_reg &= ~(1u << port_num); // need to add lock/unlock if RMW not atomically done
+
+
+  return SUCCESS;
+		       
+}
+
--- struct-util.c
+++ struct-util.c
@@ -1,8 +1,53 @@
 
-int
-set_struct_field(int struct_id, int field_offset, int field_type, void *val, int val_len)
-{
+typedef unsigned char uchar;
 
-return 0;
+
+/** @brief Sets a field in the structure passed
+ *
+ *  @param structure Pointer to the structure
+ *  @param struct_id Integer id for the structure
+ *  @param field_offset Starting offset of the field in the structure, in bytes
+ *  @param value Pointer to the value to be set in the field
+ *  @param length The length of the value passed, in bytes
+ *  @return SUCCESS or ERROR
+ */
+int
+set_struct_field(void *structure,
+		 int struct_id, 
+		 int field_offset, 
+		 void *value, 
+		 int length)
+{
+  if (!structure || !value)
+    return ERROR;
+      
+
+  memcpy (((uchar *)structure) + field_offset, value, length);
+  return 0;
+}
+
+
+
+/** @brief Gets a field in the structure passed
+ *
+ *  @param structure Pointer to the structure
+ *  @param struct_id Integer id for the structure
+ *  @param field_offset Starting offset of the field in the structure, in bytes
+ *  @param value Pointer to the value to receive the field value
+ *  @param length The length of the field value passed, in bytes
+ *  @return SUCCESS or ERROR
+ */
+int
+get_struct_field(void *structure,
+		 int struct_id, 
+		 int field_offset, 
+		 void *value, 
+		 int length)
+{
+  if (!structure || !value)
+    return ERROR;
+
+  memcpy (value, ((uchar *)structure) + field_offset, length);
+  return 0;
 }
 
